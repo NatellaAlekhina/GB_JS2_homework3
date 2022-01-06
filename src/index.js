b403d80c6f999160e96ef588baa622afc7ad6f04
Natalia <natellaozabota@gmail.com>
2022-01-06 15:43:31 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+//import hello from "./module.js" // относительный путь к файлу, из которого импортируем. назвать можно как угодно, так как экспорт был по default
+//import _ from "lodash" //подключаем библиотеку lodash через нижнее подчеркивание - _ это и есть символ лодаш
+
+//hello(_.camelCase("super_admin")) //обращаемся к библиотеке лодаш через нижнее подчеркивание и подставляем в функцию hello значение
+
+import getProductList from "./mock/data.js";
+import renderGoodsList from "./showcase.js";
+//import { send } from './utils.js';
+import css from "./style/main.css";
+
+const productList = getProductList(20);
+renderGoodsList(productList);
+
+//const API_URL = 'http://localhost:3000/api/v1'
+
+/*let productList = [];
+/let cart = [];
+
+send((error) => { console.log(err) }, (res) => { 
+  let list = JSON.parse(res);
+  productList = list;
+  renderGoodsList(productList);
+}, `${API_URL}/catalog`)
+
+
+
+// Пользователь добавляет товар в корзину
+let buyed = {id: 5, title: "new", price: 999};
+send((error) => { console.log(err) }, (res) => {
+  cart.push(buyed)
+}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), {"Content-Type": "application/json"})*/
